45881148c97dda68c043f6cb284117205a64e509
champkeh <champion.ge@gmail.com>
2022-07-17 20:56:32 +0800

    refactor: 修改basePath

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ function resolve(dir: string) {
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
-        base: "/",
+        base: "/dist",
         resolve: {
             alias: {
                 "@": resolve("src"),
