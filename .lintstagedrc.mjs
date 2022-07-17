96e7b93c550e5f34f342539d13f2eab0b5e46d52
champkeh <champion.ge@gmail.com>
2022-07-17 23:27:58 +0800

    chore: 自动化发布

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -3,5 +3,4 @@ export default {
     "*.{ts,cts,mts,vue}": [() => "vue-tsc -p tsconfig.json --noEmit"],
     "*.{js,cjs,mjs,ts,cts,mts,vue}": ["eslint --fix"],
     "*.{css,scss,vue}": ["stylelint --fix --allow-empty-input"],
-    "*.{png,jpeg,jpg,gif}": ["npm run compress"],
 }
