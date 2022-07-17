b213866c4fc53b4ee5c2ebfb1176ad4d8d74555f
champkeh <champion.ge@gmail.com>
2022-07-17 15:18:21 +0000

    Deploying to gh-pages from @ champkeh/e-calculator@6304fed39f95003f01c1fee7553f24666e3dce3d 🚀

--- .lintstagedrc.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-    "*": ["prettier --write --ignore-unknown --no-editorconfig"],
-    "*.{ts,cts,mts,vue}": [() => "vue-tsc -p tsconfig.json --noEmit"],
-    "*.{js,cjs,mjs,ts,cts,mts,vue}": ["eslint --fix"],
-    "*.{css,scss,vue}": ["stylelint --fix --allow-empty-input"],
-    "*.{png,jpeg,jpg,gif}": ["npm run compress"],
-}
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue"
-import App from "@/App.vue"
-
-// 引入全局样式
-import "@/styles/index.scss"
-
-const app = createApp(App)
-
-app.mount("#app")
